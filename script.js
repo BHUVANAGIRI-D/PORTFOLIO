f1718579baaaa721656f83c58d75aef7ef9a8318
BHUVANAGIRI <avavenum@gmail.com>
2026-04-14 14:26:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+/* CURSOR */
+const cursor=document.querySelector(".cursor");
+document.addEventListener("mousemove",e=>{
+  cursor.style.left=e.clientX+"px";
+  cursor.style.top=e.clientY+"px";
+});
+
+/* TYPING */
+const words=["Web Developer","Python Learner","AI Enthusiast"];
+let i=0,j=0,del=false;
+
+function type(){
+let cur=words[i];
+document.getElementById("typing").textContent=cur.substring(0,j);
+
+if(!del && j<cur.length){j++;}
+else if(del && j>0){j--;}
+else{del=!del;if(!del)i=(i+1)%words.length;}
+
+setTimeout(type,del?50:100);
+}
+type();
+
+/* SCROLL */
+function goProject(){
+document.getElementById("projects").scrollIntoView({behavior:"smooth"});
+}
+
+/* MUSIC */
+const music=document.getElementById("music");
+function toggleMusic(){
+if(music.paused){music.play();}
+else{music.pause();}
+}
+
+/* FADE */
+const faders=document.querySelectorAll('.fade');
+window.addEventListener('scroll',()=>{
+faders.forEach(el=>{
+const top=el.getBoundingClientRect().top;
+if(top<window.innerHeight-100){
+el.classList.add('show');
+}
+});
+});
+
+/* PARTICLES */
+const canvas=document.getElementById("particles");
+const ctx=canvas.getContext("2d");
+
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+
+let particles=[];
+
+for(let i=0;i<100;i++){
+particles.push({
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+size:Math.random()*3,
+dx:(Math.random()-0.5),
+dy:(Math.random()-0.5)
+});
+}
+
+function animate(){
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+particles.forEach(p=>{
+p.x+=p.dx;
+p.y+=p.dy;
+
+ctx.fillStyle="#ff2e2e";
+ctx.beginPath();
+ctx.arc(p.x,p.y,p.size,0,Math.PI*2);
+ctx.fill();
+});
+
+requestAnimationFrame(animate);
+}
+animate();
